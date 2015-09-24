807e42baea20a62d9c11a980a839a888e714e084
Katherine <awakekat@aol.com>
2015-09-24 16:10:32 -0400

    added php page and font awesome

--- pages.php
+++ pages.php
@@ -5,6 +5,20 @@
   <title>Page Layout</title>
 </head>
 <body>
-  
+<!-- 100% wide -->
+  <header>
+    
+  </header>
+  <div class="breadcrumb">
+    
+  </div>
+<!-- two colums 4/8 -->
+<div class="container">
+  <div class="row">
+    .col
+  </div>
+
+</div>
+
 </body>
 </html>
