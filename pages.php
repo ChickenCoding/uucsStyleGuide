0adb5c9c0901fd674dabdc70a156bc64ae249cd8
Katherine Wakefield <awakekat@aol.com>
2015-09-25 00:20:15 -0400

    deleted readme.html added class private to page.php

--- pages.php
+++ pages.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="../css/style.css">
 </head>
-<body>
+<body class="private">
 <!-- 100% wide -->
   <header>
     <h1>Hi</h1>
