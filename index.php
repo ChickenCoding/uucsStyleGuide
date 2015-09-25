192abe6286b7892cb2b8c3d910e2e0ef4d1476ac
Jerome Bienaime <jerome.bienaime@horyou.com>
2015-09-25 10:21:55 +0200

    Added basic workflow #workflow
    Installed third party libs with bower.
    Removed cdn fetch (but google fonts) in favor of local fetch.
    Added css and js on the homepage.

--- index.php
+++ index.php
@@ -3,22 +3,23 @@ require_once __DIR__ . '/vendor/autoload.php';
 $app = new Silex\Application();
 
 $app["debug"] = true;
+$app["bower"] = "client/bower_components";
+$app["css"] = "css";
+$app["js"] = "js";
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__ . '/views',
 ));
 
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-$app->register(new Silex\Provider\SessionServiceProvider());
-
 $app->get('/', function () use ($app) {
-  return $app['twig']->render('page_parts/pages.html');
+  return $app['twig']->render(
+    'page_parts/pages.html', ["title" => "Page Layout"]
+  );
 })->bind("homepage");
 
 $app->get('/styleguide', function () use ($app) {
   return $app['twig']->render('styleguide/index.html');
 })->bind("styleguide");
-
 
 $app->run();
 
---
+++ js/bxSlider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    slideWidth: 935,
+    //captions: true,
+    speed: 900,
+    auto: true,
+    //controls: false,
+    pager: false,
+    touchEnabled: false
+  });
+});
