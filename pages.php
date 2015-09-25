8ec2da37f1f570f0a9497810300816677ea74136
Katherine <awakekat@aol.com>
2015-09-24 22:22:52 -0400

    changes

--- pages.php
+++ pages.php
@@ -3,19 +3,26 @@
 <head>
   <meta charset="UTF-8">
   <title>Page Layout</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 <!-- 100% wide -->
   <header>
-    
+    <h1>Hi</h1>
   </header>
   <div class="breadcrumb">
-    
+    <div class="fa fa-home"></div>
   </div>
 <!-- two colums 4/8 -->
 <div class="container">
   <div class="row">
-    .col
+    <div class="col-md-4">
+      <!-- menu -->
+    </div>
+    <div class="col-md-8">
+      <!-- pg contents -->
+    </div>
   </div>
 
 </div>
